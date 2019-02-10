d474c42fbf14b00e55c4179cf3b11ac37c3270ae
ivetafurdova <47497791+ivetafurdova@users.noreply.github.com>
2019-02-10 14:37:10 +0200

    Add files via upload

---
+++ NewDonar.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KravnaGrupa.Model
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KravnaGrupa
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewDonar : ContentPage
+	{
+		public NewDonar ()
+		{
+			InitializeComponent ();
+		}
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Post post = new Post()
+            {
+                Experience = expretienceEntry.Tex
+            };
+            sq
+        }
+    }
+}
